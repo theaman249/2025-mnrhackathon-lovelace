f7eb056d565fb04a22913098fd1138d22b5c36d3
Agape <theaman249@gmail.com>
2025-07-29 15:48:51 +0200

    added a conn.js to connect from backend to the DB.

--- backend/commons/conn.js
+++ backend/commons/conn.js
@@ -1,1 +1,13 @@
-console.log("Vho KostiMunene");
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  password: 'admin',
+  port: 5432,
+  database: 'workbench',
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
