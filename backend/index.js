3aa2844d2d41590e488363fa7b19f71d03838ec9
Amanda9805 <akhuzwayo98@gmail.com>
2025-07-29 15:06:41 +0200

    auth

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const authRoutes = require('./routes/auth');
+const profileRoute = require('./routes/profile');
+const dashboardRoute = require('./routes/dashboard');
 
 app.use(express.json());
 
@@ -9,6 +12,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Backend is working');
 });
+
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoute);
+app.use('/dashboard', dashboardRoute);
 
 // Start the server
 app.listen(port, () => {
