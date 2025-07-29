c2c4ae762a8736754d11537d943e868c7923e435
Agape <theaman249@gmail.com>
2025-07-29 12:44:58 +0200

    Database scripts up and running

--- backend/commons/database.js
+++ backend/commons/database.js
@@ -1,1 +1,66 @@
-// for datase schema defenitions and migrations
+const { Client } = require('pg');
+
+// Database initialization
+(async () => {
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        password: 'admin',
+        port: 5432
+    });
+
+    try {
+
+        console.log(`Running Database script...`);
+
+        await client.connect();
+        
+        const dbName = 'genesis';
+
+        // Check if the database exists
+        const result = await client.query(
+            `SELECT datname FROM pg_database WHERE datname = $1`, [dbName]
+        );
+
+        if (result.rows.length > 0) {
+            console.log(`Database "${dbName}" already exists.`);
+        } 
+        else {
+            console.log(`Database "${dbName}" does not exist. Creating...`);
+            await client.query(`CREATE DATABASE ${dbName}`);
+        }
+        
+        await client.end(); 
+
+        const dbClient = new Client({
+            user: 'postgres',
+            host: 'localhost',
+            password: 'admin',
+            port: 5432,
+            database: dbName, // Now connect to "genesis"
+        });
+
+        await dbClient.connect();
+
+        // Create tables
+        await dbClient.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                surname VARCHAR(100) NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                password VARCHAR(255) NOT NULL
+            );
+            `
+        );
+
+        console.log('Table "users" created successfully.');
+
+        await dbClient.end();
+        
+        
+    } catch (error) {
+        console.error('Error:', error);
+        process.exit(1); 
+    }
+})();
---
+++ backend/commons/populate.js
@@ -0,0 +1,50 @@
+const {Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    password: 'admin',
+    port: 5432,
+    database: 'genesis'
+});
+
+(async () => {
+    try {
+
+        await client.connect();
+
+        const res = await client.query(`SELECT * FROM users`);
+
+        if (res.rows.length > 0) {
+            console.log("entries already exist");
+            await client.end();
+            process.exit(0);
+        }
+
+        // Insert default users
+        await client.query(`
+        INSERT INTO users
+            (name, surname, email, password)
+        VALUES
+            ('Jensen', 'Huang', 'jensen@workbench.co.za', 'Password@123'),
+            ('Ada', 'Lovelace', 'ada@workbench.co.za', 'Password@123'),
+            ('Stephan', 'Grunner', 'profg@workbench.co.za', 'Password@123'),
+            ('Rendani', 'Krugger', 'rendi@workbench.co.za', 'Password@123');
+        `);
+
+        const result = await client.query(`SELECT * FROM users`);
+
+        if (result.rows.length > 0) {
+            console.log("entries were successfully inserted");
+        } else {
+            console.log("could not insert entries into the database");
+        }
+        
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        res.status(500).send('Internal Server Error');
+    } finally {
+        await client.end();
+    }
+
+})();
